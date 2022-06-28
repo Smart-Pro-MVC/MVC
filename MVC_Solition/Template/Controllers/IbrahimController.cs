0387e24b7590e6b6a362a811b1c8251f832b0160
TuranEdogan <turan.erdogan@windowslive.com>
2022-06-28 15:03:59 +0300

    Listeler

--- MVC_Solition/Template/Controllers/IbrahimController.cs
+++ MVC_Solition/Template/Controllers/IbrahimController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Template.Listeler;
 
 namespace Template.Controllers
 {
@@ -11,11 +12,36 @@ namespace Template.Controllers
         // GET: Ibrahim
         public ActionResult Ibrahim()
         {
-            return View();
+            List<Urun> UrunListesi = new List<Urun>();
+            Urun Liste = new Urun();
+            Liste.Id = 1;
+            Liste.UrunAdi = "Samsung s7";
+            Liste.Fiyatı = 5000;
+            Liste.StoktaVar = true;
+            UrunListesi.Add(Liste);
+            
+            Liste.Id = 2;
+            Liste.UrunAdi = "Samsung s10";
+            Liste.Fiyatı = 6000;
+            Liste.StoktaVar = true;
+            UrunListesi.Add(Liste);
+
+            Liste.Id = 3;
+            Liste.UrunAdi = "Samsung s11";
+            Liste.Fiyatı = 7000;
+            Liste.StoktaVar = false;
+            UrunListesi.Add(Liste);
+
+
+            return View(UrunListesi.ToList());
+
+ 
         }
         public ActionResult Ibrahim2()
         {
-            return View();
+           
+            string[] Kategoriler = { "Bilgisayar", "Telefon", "Aksesuar", "Beyaz Eşya", "Ev Elektroniği","Ses Sistemi" };
+            return View(Kategoriler);
         }
     }
 }
---
+++ MVC_Solition/Template/Listeler/Urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Template.Listeler
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public double Fiyatı { get; set; }
+        public bool StoktaVar { get; set; }
+    }
+}
